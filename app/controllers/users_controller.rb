411409a6be8440b59f1b8b971213cd518f1c7377
Christopher Vundi <christophervundi@gmail.com>
2016-04-07 12:57:02 +0000

    Add episode controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,6 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
+        @episodes = @user.episodes
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'episodes/new'
+
   devise_for :users
   root 'welcome#index'
   
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :episodes
+  end
 
 end
